4aa9296e941b8a36a86b451ffdb899694d5aaf13
valikbrrr <139292164+valikbrrr@users.noreply.github.com>
2025-03-03 17:20:48 +0300

    feat: refactor OurAdvantages

--- src/pages/home/ui/sections/ourAdvantages/OurAdvantages.tsx
+++ src/pages/home/ui/sections/ourAdvantages/OurAdvantages.tsx
@@ -2,39 +2,42 @@ import { RequestIcon, StafIcon, UniformOrderIcon } from "@shared/assets";
 import styles from "./ourAdvantages.module.css";
 
 export const OurAdvantages = () => {
+  // Массив данных для карточек
+  const advantagesCards = [
+    {
+      icon: <StafIcon />,
+      text: "Квалифицированный\nперсонал",
+    },
+    {
+      icon: <RequestIcon />,
+      text: "Онлайн подача\nзаявок 24/7",
+    },
+    {
+      icon: <UniformOrderIcon />,
+      text: "Единый\nподрядчик",
+    },
+  ];
+
   return (
     <section className={styles.ourAdvantages}>
       <div className={styles.ourAdvantagesTitle}>
         <h2>Наши преимущества</h2>
       </div>
+
       <div className={styles.ourAdvantagesCards}>
-        <div className={styles.card}>
-          <div className={styles.advantagesIconBlock}>
-            <StafIcon />
+        {advantagesCards.map((card, index) => (
+          <div key={index} className={styles.card}>
+            <div className={styles.advantagesIconBlock}>{card.icon}</div>
+            <div className={styles.advantagesTextBlock}>
+              {card.text.split("\n").map((line, i) => (
+                <span key={i}>
+                  {line}
+                  <br />
+                </span>
+              ))}
+            </div>
           </div>
-          <div className={styles.advantagesTextBlock}>
-            Квалифицированный
-            <br /> персонал
-          </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.advantagesIconBlock}>
-            <RequestIcon />
-          </div>
-          <div className={styles.advantagesTextBlock}>
-            Онлайн подача
-            <br /> заявок 24/7
-          </div>
-        </div>
-        <div className={styles.card}>
-          <div className={styles.advantagesIconBlock}>
-            <UniformOrderIcon />
-          </div>
-          <div className={styles.advantagesTextBlock}>
-            Единый
-            <br /> подрядчик
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
